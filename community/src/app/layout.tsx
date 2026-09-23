3f7e0786ac697e5f5a56175e7726603e51654f02
Jose Vicente Vicedo Silla <josevicentevicedosilla@gmail.com>
2026-09-23 21:57:40 +0200

    style(community): load dual-platform download UI

--- community/src/app/layout.tsx
+++ community/src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./mobile.css";
 import "./ux.css";
 import "./game-reference-theme.css";
 import "./authored.css";
+import "./platform-downloads.css";
 import "./world-backgrounds.css";
 
 export const metadata: Metadata = {
